406facf6822a16a3d9f5774b2f2579396f627a2c
Matthieu Laurent <matthieu.laurent@etu.univ-grenoble-alpes.fr>
2024-06-05 16:33:56 +0200

    Controller fini mais pas testé

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import java.util.*;
+
 import info3.game.graphics.GameCanvasListener;
 import view.View;
 
@@ -11,10 +13,16 @@ public class Controller implements GameCanvasListener {
 
 	Controller m_controller;
 	View m_view;
+	Model m_model;
+	private long tick;
+	List keyList;
 
 	public Controller() {
 		m_controller = this;
 		m_view = new View(this);
+		m_model = new Model(this, m_view);
+		this.tick = 0;
+		this.keyList = new List<int>;
 	}
 
 	public static Controller getInstance() {
@@ -24,60 +32,75 @@ public class Controller implements GameCanvasListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("Mouse clicked: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("Mouse pressed: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("Mouse released: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("Mouse entered: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Mouse exited: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Mouse dragged: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Mouse moved: (" + e.getX() + "," + e.getY() + ")");
+		System.out.println("   modifiers=" + e.getModifiersEx());
+		System.out.println("   buttons=" + e.getButton());
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Key typed: " + e.getKeyChar() + " code=" + e.getKeyCode());
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Key pressed: " + e.getKeyChar() + " code=" + e.getKeyCode());
+		keyList.add(e.getKeyCode());
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		System.out.println("Key released: " + e.getKeyChar() + " code=" + e.getKeyCode());
+		keyList.remove(e.getKeyCode());
 
 	}
 
@@ -91,6 +114,10 @@ public class Controller implements GameCanvasListener {
 	public void tick(long elapsed) {
 		// TODO Auto-generated method stub
 		m_view.tick(elapsed);
+		if ((elapsed - tick) >= 1000) {
+			m_model.tick();
+			tick = elapsed;
+		}
 	}
 
 	@Override
