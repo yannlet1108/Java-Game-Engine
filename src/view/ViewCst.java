919266a07cf67dc7747f893e100028029e2b4a74
Chevrier--Pivot-Yohan <yohan.chevrier-pivot@etu.univ-grenoble-alpes.fr>
2024-06-05 16:34:09 +0200

    Quadrillage et affichage playground propre

--- src/view/ViewCst.java
+++ src/view/ViewCst.java
@@ -8,12 +8,20 @@ public class ViewCst {
 	public static final int WIN_HEIGHT = 768;
 
 	public static final Color BACKGROUND_COLOR = Color.GRAY;
+	public static final Color LINE_COLOR = Color.RED;
 	public static final Color PLAYGROUND_COLOR = Color.GREEN;
 
-	public static final int GAME_SQUARE = 15;
-	public static final int X_MARGIN = WIN_WIDTH - WIN_HEIGHT + GAME_SQUARE;
-	public static final int Y_MARGIN = GAME_SQUARE;
+	public static final int PLAYGROUND_WIDTH = 500;
+	public static final int PLAYGROUND_HEIGHT = 500;
+	
+	public static final int X_MARGIN = (WIN_WIDTH - PLAYGROUND_WIDTH)/2;
+	public static final int Y_MARGIN = (WIN_HEIGHT - PLAYGROUND_HEIGHT)/2;
 
-	public static final int CELL_SIZE = (WIN_WIDTH - 2 * X_MARGIN) / ModelCst.NB_CELL;
-	public static final int CELL_MARGIN = 5;
+	// a supprimer (from model)
+	public static final int NB_CELL_HEIGHT = 15;
+	public static final int NB_CELL_WIDTH = 15;
+
+	public static final int CELL_HEIGHT = PLAYGROUND_HEIGHT / NB_CELL_HEIGHT;
+	public static final int CELL_WIDTH = PLAYGROUND_WIDTH / NB_CELL_WIDTH;
+
 }
