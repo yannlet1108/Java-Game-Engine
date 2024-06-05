2a034c31361a724f5b565400d08427d033ed5f46
Gwendal Trehin <gwendal.trehin@etu.univ-grenoble-alpes.fr>
2024-06-05 16:07:58 +0200

    Création des classes du modèle et premières déclarations de fonctinos

---
+++ src/model/Entity.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Entity {
+
+	public int getX() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public int getY() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public void move() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public void egg() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public void pick() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public void explode() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public boolean cell(Direction direction, Category category) {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public Direction getDirection() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public void setDirection() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+}
---
+++ src/model/Snake.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.awt.Point;
+import java.util.List;
+
+public class Snake extends Entity {
+
+	public Snake(Point position) {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public List<Point> getListOfPositions() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+
+	public int getLength() {
+		throw new RuntimeException("Not Yet Implemented");
+	}
+}
