827fa3822b48d2f367f1144984b0dab1fa931a0f
Yann Letourneur <yann.letourneur@etu.univ-grenoble-alpes.fr>
2024-06-06 17:41:03 +0200

    Yann : Fin de journée, creation du fichier de test des automates et ajout de 2 conditions

--- src/automaton/Tests.java
+++ src/automaton/Tests.java
@@ -10,8 +10,16 @@ public class Tests {
 
 	public static void main(String[] args) {
 		
+		/*
 		Point pos = new Point(0, 0);
 		Entity snake = new Snake(pos, Direction.N);
+		
+		snake.step();
+		
+		System.out.println("Le serpent est aux coordonnées suivantes :");
+		*/
+		
+		Transition transition = new Transition(State.WAIT, new TrueCondition(), null, State.FORWARD);
 		
 	}
 	
