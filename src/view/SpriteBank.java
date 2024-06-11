b3941c1f05e616d6636f2bdff74faa562d483152
Axel Thepot <axel.thepot@etu.univ-grenoble-alpes.fr>
2024-06-11 10:40:39 +0200

    Implémentation de PlayerAvatar

---
+++ src/view/SpriteBank.java
@@ -0,0 +1,37 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * Classe utilisé comme banque de sprite pour les avatars
+ */
+public class SpriteBank {
+
+	private View m_view;
+
+	/**
+	 * Initialise la banque de sprite
+	 * 
+	 * @param m_view : instance courante de la view
+	 */
+	SpriteBank(View m_view) {
+		this.m_view = m_view;
+		m_view.setBank(this);
+	}
+
+	/**
+	 * Affiche la boite de collision de l'entité
+	 * 
+	 * @param g            : inctance graphique du canvas
+	 * @param c            : couleur de la boite de collision
+	 * @param collisionBox : coordonnées et taille de la boite de collision
+	 */
+	void debugCollisions(Graphics g, Color c, Rectangle2D collisionBox) {
+		g.setColor(c);
+		g.drawRect((int) collisionBox.getX(), (int) collisionBox.getY(), (int) collisionBox.getWidth(),
+				(int) collisionBox.getHeight());
+	}
+}
