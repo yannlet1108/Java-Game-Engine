3a13b380832ef8b118587281b595333d5d3c8728
Chevrier--Pivot-Yohan <yohan.chevrier-pivot@etu.univ-grenoble-alpes.fr>
2024-06-14 11:54:38 +0200

    ajout de l'avatar bullet

--- src/view/ViewCst.java
+++ src/view/ViewCst.java
@@ -17,5 +17,6 @@ public class ViewCst {
 	public static final Color DEBUG_OBSTACLE = Color.GREEN;
 	public static final Color DEBUG_GOLDFISH = Color.ORANGE;
 	public static final Color DEBUG_SHARK = Color.CYAN;
+	public static final Color DEBUG_BULLET = Color.DARK_GRAY;
 	
 }
