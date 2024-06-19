a62f762821c434f09558f4d1548d6c42a61fef29
Chevrier--Pivot-Yohan <yohan.chevrier-pivot@etu.univ-grenoble-alpes.fr>
2024-06-19 16:00:06 +0200

    implem animations (not tested)

--- src/view/Avatar.java
+++ src/view/Avatar.java
@@ -2,8 +2,12 @@ package view;
 
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Taskbar.State;
 import java.awt.geom.Rectangle2D;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
+import model.Direction;
 import model.Entity;
 
 /**
@@ -16,13 +20,15 @@ public abstract class Avatar {
 	protected Entity instanceEntity;
 	protected boolean isVisible;
 
-	protected int currentSpriteNumber = 0; // numero de sprite dans un set
 	protected int spriteSetNumber; // numero de set de sprite
+
+	private model.State lastState;
+
+	private Queue<Integer> animationSprite; // file de sprites pour l'animation
 
 	/**
 	 * Crée et initialise les champs communs des avatars. Doit être appellé par une
-	 * sous-classe
-	 * charge le set de sprites associé à l'entité
+	 * sous-classe charge le set de sprites associé à l'entité
 	 * 
 	 * @param m_view : instance courante de la view
 	 * @param e      : entité associé à l'avatar
@@ -35,6 +41,96 @@ public abstract class Avatar {
 				getSpritesNcols(entityType));
 
 		setInvisible();
+	}
+
+	int getNextSpriteNumber() {
+		if (animationSprite.isEmpty()) {
+			return 0;
+		}
+		return animationSprite.poll();
+	}
+
+	void updateAnimations() {
+		model.State newState = instanceEntity.getState();
+		if (newState != lastState) {
+			lastState = newState;
+			if (lastState == model.State.HITTING || lastState == model.State.HITTING_AROUND && lastState != model.State.DYING) {
+				animationSprite.clear();
+			}
+			addAnimation(lastState);
+		}
+		else {
+			if(animationSprite.isEmpty()) {
+				if(lastState!= model.State.HITTING && lastState != model.State.HITTING_AROUND && lastState != model.State.DYING) {
+					addAnimation(lastState);
+				}
+			}
+		}
+
+	}
+
+	Boolean isRightSided() {
+		Direction dir = instanceEntity.getDirection();
+		if (dir == Direction.E || dir == Direction.SE || dir == Direction.NE) {
+			return true;
+		}
+		return false;
+	}
+
+	void addAnimation(model.State state) {
+		int diff = 0;
+		if (!isRightSided()) {
+			diff = 24;
+		}
+		switch (state) {
+		case WAITING:
+			animationSprite.add(0 + diff);
+			animationSprite.add(1 + diff);
+			break;
+
+		case DYING:
+			animationSprite.add(diff + 4);
+			animationSprite.add(diff + 5);
+			animationSprite.add(diff + 6);
+			animationSprite.add(diff + 7);
+			break;
+
+		case EMPTYING:
+			animationSprite.add(diff + 20);
+			break;
+
+		case FILLING:
+			animationSprite.add(diff + 21);
+			break;
+
+		case HITTING:
+			animationSprite.add(diff + 12);
+			animationSprite.add(diff + 13);
+			animationSprite.add(diff + 14);
+			animationSprite.add(diff + 15);
+			break;
+		case HITTING_AROUND:
+			animationSprite.add(diff + 16);
+			animationSprite.add(diff + 17);
+			animationSprite.add(diff + 18);
+			animationSprite.add(diff + 19);
+			break;
+
+		case MOVING:
+			animationSprite.add(diff + 8);
+			animationSprite.add(diff + 9);
+			animationSprite.add(diff + 10);
+			animationSprite.add(diff + 11);
+			break;
+
+		case REFILLING:
+			animationSprite.add(diff + 22);
+			animationSprite.add(diff + 23);
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	/**
@@ -93,8 +189,7 @@ public abstract class Avatar {
 
 	/**
 	 * Affiche l'avatar en fonction du viewport et des coordonnées de l'entité
-	 * affichage en hitbox si mode debug activé
-	 * en sprite sinon
+	 * affichage en hitbox si mode debug activé en sprite sinon
 	 * 
 	 * @param g : instance graphique du canvas
 	 */
@@ -102,19 +197,22 @@ public abstract class Avatar {
 		if (ViewCst.DEBUG) {
 			debugPaint(g);
 		} else {
+			updateAnimations();
 			Rectangle2D collisionBox = instanceEntity.getHitbox();
 			Point origin = m_view.getViewport().toViewport(collisionBox);
 			if (origin == null) {
 				return;
 			}
-			g.drawImage(m_view.getBank().getSprite(spriteSetNumber, currentSpriteNumber), origin.x, origin.y,
+			g.drawImage(m_view.getBank().getSprite(spriteSetNumber, getNextSpriteNumber()), origin.x, origin.y,
 					(int) (collisionBox.getWidth() * m_view.getViewport().getScale()),
 					(int) (collisionBox.getHeight() * m_view.getViewport().getScale()), null);
 		}
 	}
-	
+
 	/**
-	 * affichage des hitbox de l'avatar en mode debug (differe en fonction de l'avatar)
+	 * affichage des hitbox de l'avatar en mode debug (differe en fonction de
+	 * l'avatar)
+	 * 
 	 * @param g
 	 */
 	abstract void debugPaint(Graphics g);
