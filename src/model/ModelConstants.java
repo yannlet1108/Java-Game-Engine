d3d7a3c74fd962bfc333199d08a66fdf5d099634
Moataz Errami <erramimoataz@gmail.com>
2024-06-22 18:50:58 +0200

    SpawnEnemies modified

--- src/model/ModelConstants.java
+++ src/model/ModelConstants.java
@@ -6,9 +6,4 @@ public class ModelConstants {
 
 	public static Duration PHYSICS_STEP_DELAY = Duration.ofMillis(10);
 
-
-	public static int IN_GENERAL = 1;
-	public static int GOLDENFISH_PROBA = 70; // ça signifie 70%
-	public static int SHARK_PROBA = 10;
-
 }
