320aa233e16b50c2beb08cdde98ebeb68b9d8286
Chevrier--Pivot-Yohan <yohan.chevrier-pivot@etu.univ-grenoble-alpes.fr>
2024-06-25 22:21:12 +0200

    delete storage + add config 3 4

--- storage/src/automaton/State.java
+++
@@ -1,8 +0,0 @@
-package automaton;
-
-/**
- * Classe caracterisant l'etat d'une entite
- */
-public enum State {
-	WAIT, FORWARD, DEAD;
-}
--- storage/src/model/Category.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Category {
-	VOID, OBSTACLE, SNAKE, APPLE;
-}
